23a79d68b79b20e99a6b2e2dcb933cd900f001fc
joaofp <jvfpdaemoon1@gmail.com>
2022-09-12 14:46:00 -0300

    done

--- src/controllers/itens.controller.js
+++ src/controllers/itens.controller.js
@@ -2,7 +2,6 @@ import db from '../database/db.js';
 
 async function getItens(req, res) {
     const user = res.locals.user;
-    const { id } = req.params;
     if (user) {
         try {
             const itens = await db.collection('itens').find({userId: user._id}).toArray();
